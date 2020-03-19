6d9e4e5fbcc2331f9b89ecb92b4309464b66ccac
Mateusz Tadla <mtadla@sii.pl>
2020-03-19 10:49:02 +0100

    create user factory

---
+++ AutomationTestSiiFramework.Tests/Providers/UrlProvider.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using AutomationTestSiiFramework.Extensions;
+
+namespace AutomationTestSiiFramework.Tests.Providers
+{
+    public class UrlProvider
+    {
+        private static Uri BaseUrl => new Uri(TestSettings.ConfigurationJson.ShopAppUrl);
+        public static Uri Home => BaseUrl;
+        public static Uri Cart => new Uri(BaseUrl, "cart?action=show");
+    }
+}
