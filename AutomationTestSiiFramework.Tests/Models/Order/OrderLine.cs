843168b277ca9d271073541451be20fa0d6e110d
Mateusz Tadla <mtadla@sii.pl>
2020-03-19 10:48:43 +0100

    add models for order and user

---
+++ AutomationTestSiiFramework.Tests/Models/Order/OrderLine.cs
@@ -0,0 +1,15 @@
+﻿namespace AutomationTestSiiFramework.Tests.Models.Order
+{
+    public class OrderLine
+    {
+        public Product Product { get; set; }
+        public int Quantity { get; set; }
+        public decimal TotalPrice { get; set; }
+
+        public void AddOne()
+        {
+            Quantity++;
+            TotalPrice = Product.Price * Quantity;
+        }
+    }
+}
