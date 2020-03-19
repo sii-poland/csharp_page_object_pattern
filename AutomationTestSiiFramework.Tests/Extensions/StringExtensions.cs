01e0fe4ef224a122cc66961a6e161c137b373894
Mateusz Tadla <mtadla@sii.pl>
2020-03-19 10:51:55 +0100

    string extensions

---
+++ AutomationTestSiiFramework.Tests/Extensions/StringExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Globalization;
+
+namespace AutomationTestSiiFramework.Tests.Extensions
+{
+    public static class StringExtensions
+    {
+        public static decimal ToPrice(this string text)
+        {
+            return decimal.Parse(text, NumberStyles.Currency, new NumberFormatInfo
+            {
+                CurrencyDecimalSeparator = ".",
+                CurrencyGroupSeparator = ",",
+                CurrencySymbol = "$"
+            });
+        }
+    }
+}
