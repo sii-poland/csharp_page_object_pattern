7ab95c0acbea3a43f98234a23371601ea85aed8f
Mateusz Tadla <mtadla@sii.pl>
2020-03-19 10:47:36 +0100

    base update

--- AutomationTestSiiFramework/Base/BaseTest.cs
+++ AutomationTestSiiFramework/Base/BaseTest.cs
@@ -1,37 +1,32 @@
 ﻿using AutomationTestSiiFramework.Extensions;
 using LLibrary;
-using NUnit.Allure.Core;
 using NUnit.Framework;
 using OpenQA.Selenium;
-using SimpleInjector;
 
 namespace AutomationTestSiiFramework.Base
 {
     [TestFixture]
-    [AllureNUnit]
+    // [AllureNUnit]
     public class BaseTest
     {
         [SetUp]
         public void Setup()
         {
-            Container = new Container();
             var driverFactory = new WebDriverFactory();
             var logger = new L();
-            var driver = TestSettings.ConfigurationJson.BrowserType == "local"
-                ? driverFactory.GetWebDriver(TestSettings.ConfigurationJson.BrowserName, logger)
-                : driverFactory.GetRemoteDriver(TestSettings.ConfigurationJson.BrowserName,
-                    TestSettings.ConfigurationJson.GridUrl);
-            Container.RegisterInstance(driver);
-            driver.Manage().Window.Maximize();
+            Driver = TestSettings.ConfigurationJson.BrowserType == "local"
+                ? driverFactory.GetWebDriver(logger)
+                : driverFactory.GetRemoteDriver();
+            Driver.Manage().Window.Maximize();
+            Driver.Open(TestSettings.ConfigurationJson.ShopAppUrl);
         }
 
         [TearDown]
         public void TearDown()
         {
-            var driver = Container.GetInstance<IWebDriver>();
-            driver.Quit();
+            Driver.Quit();
         }
 
-        protected Container Container;
+        protected IWebDriver Driver { get; set; }
     }
 }
